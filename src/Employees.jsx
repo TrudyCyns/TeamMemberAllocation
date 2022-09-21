b83a55fcf8b1a23897720b7ceb8d4ceefc6754ca
Gertrude Nabasirye <43541075+TrudyCyns@users.noreply.github.com>
2022-09-21 12:39:31 +0000

    Initial commit

---
+++ src/Employees.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+const Employees = () => {
+  const [employees, setEmployees] = useState([{id:1, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamA" },{id:2, fullName: "Jane Smith", designation: "Node Dev", gender: "female", teamName: "TeamA" },{id:3, fullName: "Gail Shepherd", designation: "Java Dev", gender: "female", teamName: "TeamA" }, {id:4, fullName: "Jill Bailey", designation: "React Dev", gender: "male", teamName: "TeamB" }, {id:5, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamB" }, {id:6, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamB" }, {id:7, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamB" }])
+   return (
+    <main>
+      
+    </main>
+   )
+ }
+
+export default Employees
