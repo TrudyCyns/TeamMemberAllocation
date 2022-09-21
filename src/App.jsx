51502fffa708718d8ba6ed0021c23adb43d9601e
Gertrude Nabasirye <43541075+TrudyCyns@users.noreply.github.com>
2022-09-21 16:13:24 +0000

    Set up and worked with states in Employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import './App.css'
+import { useState } from 'react';
 import Header from './Header'
-import Content from './Content'
+import Employees from './Employees'
 import Footer from './Footer'
 
 export default function App() {
+
+  const [selectedTeam, setTeam] = useState("TeamB")
+
+  const [employees, setEmployees] = useState([
+    { id: 1, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamA" },
+    { id: 2, fullName: "Jane Smith", designation: "Node Dev", gender: "female", teamName: "TeamB" },
+    { id: 3, fullName: "Gail Shepherd", designation: "Java Dev", gender: "female", teamName: "TeamC" },
+    { id: 4, fullName: "Jill Bailey", designation: "React Dev", gender: "female", teamName: "TeamD" },
+    { id: 5, fullName: "Ben Butter", designation: "Python Dev", gender: "male", teamName: "TeamA" },
+    { id: 6, fullName: "Chris Bishop", designation: "Vue Dev", gender: "male", teamName: "TeamB" },
+    { id: 7, fullName: "Drew Brent", designation: "Node Dev", gender: "female", teamName: "TeamC" }])
+
+  function handleTeamSelectionChange(event) {
+    setTeam(event.target.value)
+  }
+
+  function handleEmployeeCardClick(event) {
+    // Card clicked is changed according to team selected. teamName property will be changed to selectedTeam. IF employee is member of team, there'll be a box shadow. The teamName will then change to empty and the box shadow will be removed. New array assigned to transformedEmplyees and changes state of employees Array.
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id) ? (employee.teamName === selectedTeam) ? { ...employee, teamName: '' } : { ...employee, teamName: selectedTeam } : employee)
+    setEmployees(transformedEmployees)
+  }
+
   return (
     <div>
-      <Header/>
-      <Content/>
-      <Footer/>
+      <Header />
+      <Employees />
+      <Footer />
     </div>
   )
 }
--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,12 +1,44 @@
-import {useState} from 'react';
+
+import femaleProfile from './images/femaleProfile.jpg'
+import maleProfile from './images/maleProfile.jpg'
 
 const Employees = () => {
-  const [employees, setEmployees] = useState([{id:1, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamA" },{id:2, fullName: "Jane Smith", designation: "Node Dev", gender: "female", teamName: "TeamA" },{id:3, fullName: "Gail Shepherd", designation: "Java Dev", gender: "female", teamName: "TeamA" }, {id:4, fullName: "Jill Bailey", designation: "React Dev", gender: "male", teamName: "TeamB" }, {id:5, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamB" }, {id:6, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamB" }, {id:7, fullName: "Jim Sechen", designation: "JS Dev", gender: "male", teamName: "TeamB" }])
-   return (
-    <main>
-      
+
+  
+
+  return (
+    <main className='container'>
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-6'>
+          <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
+            <option value='TeamA'>TeamA</option>
+            <option value='TeamB'>TeamB</option>
+            <option value='TeamC'>TeamC</option>
+            <option value='TeamD '>TeamD</option>
+          </select>
+        </div>
+      </div>
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-11'>
+          <div className='card-collection'>
+            {
+              employees.map((employee) => (
+                <div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
+                  {(employee.gender === 'male') ? <img src={maleProfile} className='card-img-top' /> : <img src={femaleProfile} className='card-img-top' />}
+                  <div className='card-body'>
+                    <h5 className='card-title'>Full Name: {employee.fullName}</h5>
+                    <p className='card-text'><b>Designation: </b>{employee.designation}</p>
+                  </div>
+
+                </div>
+
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </main>
-   )
- }
+  )
+}
 
 export default Employees
